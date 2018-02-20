bd9d63731378527ecb8069a3f47b568713db5a6a
Nismah Saleem <nismah_saleem@diyatech.com>
2018-02-20 17:34:10 +0500

    FileDependency sample added for dotnetcore.

---
+++ Samples/dotnetcore/Dependencies/FileBasedDependency/FileDependency/FileDependency.cs
@@ -0,0 +1,114 @@
+﻿// ===============================================================================
+// Alachisoft (R) NCache Sample Code.
+// ===============================================================================
+// Copyright © Alachisoft.  All rights reserved.
+// THIS CODE AND INFORMATION IS PROVIDED "AS IS" WITHOUT WARRANTY
+// OF ANY KIND, EITHER EXPRESSED OR IMPLIED, INCLUDING BUT NOT
+// LIMITED TO THE IMPLIED WARRANTIES OF MERCHANTABILITY AND
+// FITNESS FOR A PARTICULAR PURPOSE.
+// ===============================================================================
+
+using System;
+using System.Threading;
+using Alachisoft.NCache.Runtime;
+using Alachisoft.NCache.Sample.Data;
+using Alachisoft.NCache.Web.Caching;
+using Alachisoft.NCache.Runtime.Dependencies;
+using System.Configuration;
+
+namespace Alachisoft.NCache.Samples
+{
+    /// <summary>
+    /// This class implements file based dependency
+    /// </summary>
+	public class FileBasedDependency
+	{
+        private static Cache _cache;
+
+        /// <summary>
+        /// Executing this method will perform all the operations to use file based dependency
+        /// </summary>
+		public static void Run()
+		{
+			// Initialize cache
+            InitializeCache();
+
+            // Add file based dependency 
+            AddFileBasedDependency();
+
+            // Dispose the cache once done
+			_cache.Dispose();
+		}
+
+        /// <summary>
+        /// This method initializes the cache
+        /// </summary>
+        private static void InitializeCache()
+        {
+            string cache = ConfigurationManager.AppSettings["CacheID"];
+
+            if (String.IsNullOrEmpty(cache))
+            {
+                Console.WriteLine("The CacheID cannot be null or empty.");
+                return;
+            }
+
+            // Initialize an instance of the cache to begin performing operations:
+            _cache = NCache.Web.Caching.NCache.InitializeCache(cache);
+
+            // Print output on console
+            Console.WriteLine(string.Format("\nCache '{0}' is initialized.", cache));
+        }
+
+        /// <summary>
+        /// This method adds file based dependency 
+        /// </summary>
+        private static void AddFileBasedDependency()
+        {
+            string dependecyfile = Environment.GetEnvironmentVariable("NCHOME") + @"\Samples\dotnet\Dependencies\FileBasedDependency\DependencyFile\foobar.txt";
+
+            // Generate a new instance of product 
+            Product product = new Product { Id = 52, Name = "Filo Mix", Category = "Grains/Cereals", UnitPrice = 46 };
+
+            // Adding item dependent on file 
+            _cache.Add("Product:52", product, new FileDependency(dependecyfile), Cache.NoAbsoluteExpiration, Cache.NoSlidingExpiration, CacheItemPriority.Normal);
+
+            Console.WriteLine("\nItem 'Product:52' added to cache with file dependency. ");
+
+            // To verify that the dependency is working, uncomment the following code.
+            // Any change in the file will cause invalidation of cache item and thus the item will be removed from cache.
+            // Following code modifies the file and then verifies the existence of item in cache.
+
+            //// Modify file programmatically
+            //ModifyDependencyFile(dependecyfile);
+            
+            ////... and then check for its existence
+            //object item = _cache.Get("Product:52");
+            //if (item == null)
+            //{
+            //    Console.WriteLine("Item has been removed due to file dependency.");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("File based dependency did not work. Dependency file located at " + dependecyfile + " might be missing or file not changed within the given interval.");
+            //}
+
+            // Remove sample data from Cache to 
+            _cache.Delete("Product:52");
+        }
+
+        /// <summary>
+        /// This method modifies dependency file programmatically
+        /// </summary>
+        /// <param name="path"> Absolute path of dependency file </param>
+        private static void ModifyDependencyFile(string path)
+        {
+            using (System.IO.StreamWriter streamWriter = new System.IO.StreamWriter(path, true))
+            {
+                streamWriter.WriteLine(string.Format("\n{0}\tFile is modifed. ", DateTime.Now));
+            }
+
+            Console.WriteLine(string.Format("File '{0}' is modified programmatically.", path));
+        }
+	}
+}
---
+++ Samples/dotnetcore/Dependencies/FileBasedDependency/README.md
@@ -0,0 +1,55 @@
+# FILE DEPENDENCY
+
+### Table of contents
+
+* [Introduction](#introduction)
+* [Prerequisites](#prerequisites)
+* [Build and Run the sample](#build-and-run-the-sample)
+* [Additional Resources](#additional-resources)
+* [Technical Support](#technical-support)
+* [Copyrights](#copyrights)
+
+### Introduction
+
+- This sample explains how NCache supports dependent items to be added, and what is the behaviour if dependency has a change.
+- Sample adds one item dependent upon a file, 
+- Any modification in the file will result in the invalidation of the item and thus the item will be removed from the cache.
+- This sample deals with the File dependency feature of NCache.
+	
+### Prerequisites
+
+- Visual Studio 2017 or later.
+- .NETCore 2.0 or later.
+
+Before the sample application is executed make sure that:
+
+- app.config have been changed according to the configurations. 
+	- Change the cache name
+	
+- By default this sample uses 'myPartitionedCache', make sure that cache is running. 
+- To verify that the dependency is working uncomment the required code from AddFileBasedDependency() method in FileDependency.cs
+
+### Build and Run the Sample
+    
+- Run the sample application.
+
+### Additional Resources
+
+##### Documentation
+The complete online documentation for NCache is available at:
+http://www.alachisoft.com/resources/docs/#ncache
+
+##### Programmers' Guide
+The complete programmers guide of NCache is available at:
+http://www.alachisoft.com/resources/docs/ncache/ncache-programmers-guide.pdf
+
+### Technical Support
+
+Alachisoft [C] provides various sources of technical support. 
+
+- Please refer to http://www.alachisoft.com/support.html to select a support resource you find suitable for your issue.
+- To request additional features in the future, or if you notice any discrepancy regarding this document, please drop an email to [support@alachisoft.com](mailto:support@alachisoft.com).
+
+### Copyrights
+
+[C] Copyright 2018 Alachisoft 
