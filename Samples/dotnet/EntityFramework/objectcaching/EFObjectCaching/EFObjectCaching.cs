efb000f8cc1a5d3936d74777dc2daa4384fe8434
Muhammad Nouman Yasin <nouman_yasin@diyatech.com>
2018-02-20 18:33:26 +0500

    NCache Entity Framework Object Caching Sample

---
+++ Samples/dotnet/EntityFramework/objectcaching/EFObjectCaching/EFObjectCaching.cs
@@ -0,0 +1,145 @@
+﻿// ===============================================================================
+// Alachisoft (R) NCache Sample Code
+// ===============================================================================
+// Copyright © Alachisoft.  All rights reserved.
+// THIS CODE AND INFORMATION IS PROVIDED "AS IS" WITHOUT WARRANTY
+// OF ANY KIND, EITHER EXPRESSED OR IMPLIED, INCLUDING BUT NOT
+// LIMITED TO THE IMPLIED WARRANTIES OF MERCHANTABILITY AND
+// FITNESS FOR A PARTICULAR PURPOSE.
+// ===============================================================================
+
+using System;
+using System.Linq;
+using Alachisoft.NCache.Web.Caching;
+using System.Configuration;
+
+namespace Alachisoft.NCache.Samples
+{
+    /// <summary>
+    /// Class that provides the functionality of the sample
+    /// </summary>
+    class EFObjectCaching
+    {
+        private static Cache _cache;
+        private static NorthwindEntities _database;
+
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        public static void Run()
+        {
+            // Initialize cache 
+            InitializeCache();
+
+            // Initialize customer factory
+            _database = new NorthwindEntities();
+
+            string customerId = "Alfki";
+
+            //Fetch customer; first time it will be loaded from database
+            Customer customer = LoadCustomer(customerId);
+
+            //this call will load customer from cache
+            customer = LoadCustomer(customerId);
+
+            //Let's update the customer
+            customer.Country = "France";
+            UpdateCustomer(customer);
+        }
+
+        /// <summary>
+        /// This method initializes the cache
+        /// </summary>
+        private static void InitializeCache()
+        {
+            string cache = ConfigurationManager.AppSettings["CacheID"];
+
+            if (String.IsNullOrEmpty(cache))
+            {
+                Console.WriteLine("The CacheID cannot be null or empty.");
+                return;
+            }
+
+            // Initialize an instance of the cache to begin performing operations:
+            _cache = NCache.Web.Caching.NCache.InitializeCache(cache);
+
+            // Print output on console
+            Console.WriteLine(string.Format("\nCache '{0}' is initialized.", cache));
+        }
+
+        /// <summary>
+        /// Loads customer for a given customer id. First it looks up from cache. if not found it fetches from database
+        /// and puts it to the cache.
+        /// </summary>
+        /// <param name="customerId">customer's id</param>
+        /// <returns></returns>
+        private static Customer LoadCustomer(string customerId)
+        {
+            if (string.IsNullOrEmpty(customerId))
+                throw new ArgumentException("customerId is null or an empty string");
+
+            Customer customer = null;
+
+            //First try to fech item from cache
+            string cacheKey = "Customer#" + customerId;
+            customer = _cache.Get(cacheKey) as Customer;
+
+            //if cache miss occurs, fetch from database and put into the cache; next calls to this method will be served from cache
+            if (customer == null)
+            {
+                Console.WriteLine("Customer loaded from database sucessfully.");
+                var query = from c in _database.Customers where c.CustomerID == customerId select c;
+
+                customer = query.First();
+
+                //Insert data into the cache
+                if(customer != null)
+                {
+                    CacheItem cacheItem = new CacheItem(customer);
+                    //set absolute expiration of 30 seconds. 
+                    cacheItem.AbsoluteExpiration = DateTime.Now.Add(new TimeSpan(0,0,30));
+
+                    _cache.Insert(cacheKey,cacheItem);
+                }
+            }
+            else
+            {
+
+                Console.WriteLine("Customer loaded from cache sucessfully.");
+                //add or attach customer object with database context
+                if (!_database.Customers.Any(p => p.CustomerID == customer.CustomerID))
+                    _database.Customers.Attach(customer);
+            }
+
+            return customer;
+        }
+
+        /// <summary>
+        /// This method updates customer and puts it to the cache.
+        /// </summary>
+        /// <param name="customerId">customer instance</param>
+        private static void UpdateCustomer(Customer customer)
+        {
+            if (customer == null)
+                throw new ArgumentNullException("customer");
+
+            //update item into the database
+            _database.SaveChanges();
+
+            //now update customer object in cache
+            string cacheKey = "Customer#" + customer.CustomerID;
+
+            CacheItem cacheItem = new CacheItem(customer);
+            //set absolute expiration of 30 seconds. 
+            cacheItem.AbsoluteExpiration = DateTime.Now.Add(new TimeSpan(0, 0, 30));
+            _cache.Insert(cacheKey, cacheItem);
+
+            Console.WriteLine("Customer is successfully updated in cache.");
+        }
+    }
+}
+
+
+
+
+
---
+++ Samples/dotnet/EntityFramework/objectcaching/README.md
@@ -0,0 +1,57 @@
+# EFObjectCaching
+
+### Table of contents
+
+* [Introduction](#introduction)
+* [Prerequisites](#prerequisites)
+* [Build and Run the sample](#build-and-run-the-sample)
+* [Additional Resources](#additional-resources)
+* [Technical Support](#technical-support)
+* [Copyrights](#copyrights)
+
+### Introduction
+
+- This is a sample application based on NCache's EFCaching provider. NCache's EFObjectCaching implementation allows you to store your entity data to NCache’s Distributed/OutProc cache. This sample cache entity objects using API. 
+
+- EFCaching uses configuration from [InstallDir]\config\efcaching.ncconf. A more detailed information can be found in NCache Documentation.
+
+### Prerequisites
+
+Following are the prerequisites for this sample.
+
+- Northwind is configured in SqlServer. 
+- Entity Framework 6.1.3
+
+Before the sample application is executed make sure that:
+
+- app.config have been changed according to the configurations. 
+	- Change the cache name 
+	- Change connection string for the SQL database in app.config.
+- Make sure SQL service is running and server host Northwind sample database.
+- Verify that app-id is same as specified in efcaching.config in the configuration folder **[InstallDir]\config**.
+- By default this sample uses 'mycache', make sure that cache is running. 
+
+### Build and Run the Sample
+    
+- Run the sample application.
+
+### Additional Resources
+
+##### Documentation
+The complete online documentation for NCache is available at:
+http://www.alachisoft.com/resources/docs/#ncache
+
+##### Programmers' Guide
+The complete programmers guide of NCache is available at:
+http://www.alachisoft.com/resources/docs/ncache/ncache-programmers-guide.pdf
+
+### Technical Support
+
+Alachisoft [C] provides various sources of technical support. 
+
+- Please refer to http://www.alachisoft.com/support.html to select a support resource you find suitable for your issue.
+- To request additional features in the future, or if you notice any discrepancy regarding this document, please drop an email to [support@alachisoft.com](mailto:support@alachisoft.com).
+
+### Copyrights
+
+[C] Copyright 2017 Alachisoft 
