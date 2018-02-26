e77dcf697f4a7678896b8c9c895e9d0969da9a8c
Latif Saif Talpur <latif_saif@diyatech.com>
2018-02-26 12:30:44 +0500

    updated the installation scripts.

--- Scripts/Installation-scripts/install-scripts/copy-files.bat
+++ Scripts/Installation-scripts/install-scripts/copy-files.bat
@@ -3,71 +3,71 @@
  REG QUERY "HKLM\SOFTWARE\Microsoft\NET Framework Setup\NDP\v4\full" /v InstallPath >nul 2>&1
  SET QUERY_STATUS=%ERRORLEVEL%
  SET ZERO=0
+ @Set directory=%~1
  IF %QUERY_STATUS%==%ZERO% ( 
  
 	ECHO COPYING SERVICE...
 	ECHO ==================
-	XCOPY "..\..\..\Src\build\Server\4x\Alachisoft.NCache.Service.exe" %1\bin\service\ /Y /Q
-	XCOPY "..\..\..\Src\build\Server\4x\Alachisoft.NCache.CacheHost.exe" %1\bin\service\ /Y /Q
-	XCOPY "..\..\..\Samples\config\Alachisoft.NCache.CacheHost.exe.config" %1\bin\service\ /Y /Q
-	XCOPY "..\..\..\Samples\config\Alachisoft.NCache.Service.exe.config" %1\bin\service\ /Y /Q
+	XCOPY "..\..\Src\build\Server\4x\Alachisoft.NCache.Service.exe" "%directory%\bin\service\" /Y /Q
+	XCOPY "..\..\Src\build\Server\4x\Alachisoft.NCache.CacheHost.exe" "%directory%\bin\service\" /Y /Q
+	XCOPY "setup-utilities\Alachisoft.NCache.CacheHost.exe.config" "%directory%\bin\service\" /Y /Q
+	XCOPY "setup-utilities\Alachisoft.NCache.Service.exe.config" "%directory%\bin\service\" /Y /Q
 	ECHO.
 	
 	
 	ECHO COPYING DLLS...
 	ECHO ===============
-	XCOPY "..\..\..\Resources\ncregistry.dll" %WINDIR%\System32\ /Y /Q
+	XCOPY "..\..\Resources\ncregistry.dll" %WINDIR%\System32\ /Y /Q
 	
-	FOR %%f IN (..\..\..\Src\build\Server\4x\*NCache*.dll) DO (
-			XCOPY "%%f" %1\bin\assembly\4.0\ /Y /Q /D	
+	FOR %%f IN (..\..\Src\build\Server\4x\*NCache*.dll) DO (
+			XCOPY "%%f" "%directory%\bin\assembly\4.0\" /Y /Q /D	
 	) 
-	FOR %%f IN (..\..\..\Src\build\NetCore\Client\Alachisoft*.dll) DO (
-			XCOPY "%%f" %1\bin\assembly\netcore20\ /Y /Q /D	
+	FOR %%f IN (..\..\Src\build\NetCore\Client\Alachisoft*.dll) DO (
+			XCOPY "%%f" "%directory%\bin\assembly\netcore20\" /Y /Q /D	
 	) 
-	IF EXIST %1\bin\assembly\4.0 (
-		XCOPY "..\..\..\Resources\64bitOracle11g\Oracle.ManagedDataAccess.dll" %1\bin\assembly\4.0\ /Y /Q
-		XCOPY "..\..\..\Resources\protobufdotnet\x32\protobuf-net.dll" %1\bin\assembly\4.0\ /Y /Q
-		XCOPY "..\..\..\Resources\log4net\log4net.dll" %1\bin\assembly\4.0\ /Y /Q
-		XCOPY "..\..\..\Resources\snmp\SharpSnmpLib.dll" %1\bin\assembly\4.0\ /Y /Q
-		XCOPY "..\..\..\Resources\ssh.net\Renci.SshNet.dll" %1\bin\assembly\4.0\ /Y /Q
+	IF EXIST %directory%\bin\assembly\4.0 (
+		XCOPY "..\..\Resources\64bitOracle11g\Oracle.ManagedDataAccess.dll" "%directory%\bin\assembly\4.0\" /Y /Q
+		XCOPY "..\..\Resources\protobufdotnet\x32\protobuf-net.dll" "%directory%\bin\assembly\4.0\" /Y /Q
+		XCOPY "..\..\Resources\log4net\log4net.dll" "%directory%\bin\assembly\4.0\" /Y /Q
+
 	)
 	ECHO.
 
 	ECHO COPYING TOOLS...
 	ECHO ================
-	FOR %%f IN (..\..\..\Tools\build\Server\4.0\*.exe) Do (
-		XCOPY "%%f" %1\bin\tools\ /Y /Q
+	FOR %%f IN (..\..\Tools\build\Server\4.0\*.exe) Do (
+		XCOPY "%%f" "%directory%\bin\tools\" /Y /Q
 	)
-	XCOPY "..\..\..\Tools\build\Server\4x\ncacheps.dll" %1\bin\tools\ncacheps\ /Y /Q
+	XCOPY "..\..\Tools\build\Server\4x\ncacheps.dll" "%directory%\bin\tools\ncacheps\" /Y /Q
 	ECHO.
 	
 	ECHO COPYING INTEGRATIONS ...
 	ECHO ========================
-	XCOPY "..\..\..\Integration\build\ContentOptimization" %1\integrations\ContentOptimization\ /I /Y /Q /S
-	XCOPY "..\..\..\Integration\build\LINQToNCache\4.0" %1\integrations\LINQToNCache\4.0 /I /Y /Q /S
-	XCOPY "..\..\..\Integration\build\LINQToNCache\dotnetcore" %1\integrations\LINQToNCache\dotnetcore\ /I /Y /Q /S
-	XCOPY "..\..\..\Integration\build\NHibernate" %1\integrations\nhibernate\ /I /Y /Q /S
-	XCOPY "..\..\..\Integration\build\NCache.SignalR" %1\integrations\NCache.SignalR\ /I /Y /Q /S
-	XCOPY "..\..\..\Integration\build\PowerShell\Scripts" %1\integrations\PowerShell\ /I /Y /Q /S
-	XCOPY "..\..\..\Integration\build\MSEntityFramework" %1\integrations\MSEntityFramework\ /I /Y /Q /S
-	XCOPY "..\..\..\Integration\build\OutputCache" %1\integrations\OutputCache\ /I /Y /Q /S
+	XCOPY "..\..\Integration\build\ContentOptimization" "%directory%\integrations\ContentOptimization\" /I /Y /Q /S
+	XCOPY "..\..\Integration\build\LINQToNCache\4.0" "%directory%\integrations\LINQToNCache\" /I /Y /Q /S
+	XCOPY "..\..\Integration\build\LINQToNCache\dotnetcore" "%directory%\integrations\LINQToNCache\dotnetcore\" /I /Y /Q /S
+	XCOPY "..\..\Integration\build\NHibernate" "%directory%\integrations\nhibernate\" /I /Y /Q /S
+	XCOPY "..\..\Integration\build\NCache.SignalR" "%directory%\integrations\NCache.SignalR\" /I /Y /Q /S
+	XCOPY "..\..\Integration\build\PowerShell\Scripts" "%directory%\integrations\PowerShell\" /I /Y /Q /S
+	XCOPY "..\..\Integration\build\MSEntityFramework" "%directory%\integrations\MSEntityFramework\" /I /Y /Q /S
+	XCOPY "..\..\Integration\build\OutputCache" "%directory%\integrations\OutputCache\" /I /Y /Q /S
 	
 	ECHO.
 	
 	ECHO COPYING CONFIGS...
 	ECHO ==================
-	XCOPY "..\setup-utilities\config\client.ncconf" %1\config\ /Y /Q
-	XCOPY "..\setup-utilities\config\config.ncconf" %1\config\ /Y /Q
-	XCOPY "..\setup-utilities\config\efcaching.ncconf" %1\config\ /Y /Q
+	XCOPY ".\setup-utilities\client.ncconf" "%directory%\config\" /Y /Q
+	XCOPY ".\setup-utilities\config.ncconf" "%directory%\config\" /Y /Q
+	XCOPY ".\setup-utilities\efcaching.ncconf" "%directory%\config\" /Y /Q
 	ECHO.
 	
 	ECHO License and README Files...
 	ECHO ==================
-	XCOPY "..\..\..\LICENSE" %1\ /Y /Q
-	XCOPY "..\..\..\Scripts\README.md" %1\ /Y /Q
+	XCOPY "..\..\LICENSE" "%directory%\" /Y /Q
+	XCOPY "..\..\Scripts\README.md" "%directory%\" /Y /Q
 	ECHO.
 	
-	IF NOT EXIST %1\log-files ( MKDIR %1\log-files )
+	IF NOT EXIST "%directory%\log-files" ( MKDIR "%directory%\log-files" )
  )
 
 
